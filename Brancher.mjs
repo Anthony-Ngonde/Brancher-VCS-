20f74ceccdb7976e776ef5adda1861d33cf33808
Anthony Ngonde <anto.ngonde@gmail.com>
2024-12-04 13:01:09 +0300

    Already initialised the .brancher folder

--- Brancher.mjs
+++ Brancher.mjs
@@ -1,8 +1,25 @@
 import path from 'path'
+import fs from 'fs/promises'
 
 class Brancher{
 
     constructor(repoPath = '.') {
         this.repoPath = path.join(repoPath, '.brancher');
+        this.objetcsPath = path.join(this.repoPath, 'objects'); // .brancher/objetcs
+        this.headPath = path.join(this.repoPath, 'HEAD'); // .brancher/HEAD
+        this.indexPath = path.join(this.repoPath, 'index');
+        this.init(); // .brancher/index
+    }
+
+    async init() {
+        await fs.mkdir(this.objetcsPath, {recursive: true});
+        try {
+            await fs.writeFile(this.headPath, '', {flag: 'wx'});  // wx: open for writing. fails if file exists
+            await fs.writeFile(this.indexPath, JSON.stringify([]), {flag: 'wx'});
+        } catch(error) {
+            console.log("Already initialised the .brancher folder");
+        }
     }
 }
+
+const brancher = new Brancher();
